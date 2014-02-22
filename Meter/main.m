cbfa5cc01218779083f0b0165ded1e43140437bd
Ben Redfield <bredfield@gmail.com>
2014-02-22 15:17:01 -0500

    Initial Commit

---
+++ Meter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Meter
+//
+//  Created by Ben Redfield on 2/22/14.
+//  Copyright (c) 2014 Rounded. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
